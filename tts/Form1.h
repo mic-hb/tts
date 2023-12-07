1650cf5fbb1b6f9009ad18d92af5f924cd9812e4
mic-hb <michael.h22@mhs.istts.ac.id>
2023-12-07 09:04:05 +0700

    Modified puzzle initialization

--- tts/Form1.h
+++ tts/Form1.h
@@ -1,136 +1,3 @@
-////#include "stdafx.h"
-//#include <string>
-//#include <vector>
-//#include <Windows.h>
-//#include "PuzzleLetter.h"
-//
-//#pragma once
-//
-//namespace tts {
-//
-//	using namespace System;
-//	using namespace System::ComponentModel;
-//	using namespace System::Collections;
-//	using namespace System::Windows::Forms;
-//	using namespace System::Data;
-//	using namespace System::Drawing;
-//
-//	/// <summary>
-//	/// Summary for Form1
-//	/// </summary>
-//	public ref class Form1 : public System::Windows::Forms::Form
-//	{
-//	public:
-//		Form1(void)
-//		{
-//			InitializeComponent();
-//			//
-//			//TODO: Add the constructor code here
-//			//
-//			Text = "Word Puzzle Game";
-//			Size = System::Drawing::Size(200, 200);
-//			InitializePuzzleGrid();
-//			InitializeHiddenWords();
-//		}
-//
-//	protected:
-//		/// <summary>
-//		/// Clean up any resources being used.
-//		/// </summary>
-//		~Form1()
-//		{
-//			if (components)
-//			{
-//				delete components;
-//			}
-//		}
-//
-//	private:
-//		/// <summary>
-//		/// Required designer variable.
-//		/// </summary>
-//		System::ComponentModel::Container^ components;
-//		array<PuzzleLetter^, 2>^ puzzleGrid;
-//		array<String^>^ hiddenWords;
-//		array<String^>^ foundWords;
-//
-//		void InitializePuzzleGrid() {
-//			puzzleGrid = gcnew array<PuzzleLetter^, 2>(3, 3);
-//
-//			// Initialize puzzle grid with letters
-//			for (int i = 0; i < 3; i++) {
-//				for (int j = 0; j < 3; j++) {
-//					puzzleGrid[i, j] = gcnew PuzzleLetter();
-//					puzzleGrid[i, j]->Text = Convert::ToChar('A' + i * 3 + j).ToString();
-//					//puzzleGrid[i, j]->Location = Point(50 * j, 50 * i);
-//					puzzleGrid[i, j]->Location = Point(50 * (j + 1), 50 * (i + 1));
-//					puzzleGrid[i, j]->Click += gcnew EventHandler(this, &Form1::PuzzleLetter_Click);
-//					Controls->Add(puzzleGrid[i, j]);
-//				}
-//			}
-//		}
-//
-//		void InitializeHiddenWords() {
-//			hiddenWords = gcnew array<String^>{"CAT", "DOG", "RAT"};
-//			foundWords = gcnew array<String^>(hiddenWords->Length);
-//		}
-//
-//		void PuzzleLetter_Click(Object^ sender, EventArgs^ e) {
-//			PuzzleLetter^ clickedLetter = dynamic_cast<PuzzleLetter^>(sender);
-//
-//			if (!clickedLetter->IsHighlighted) {
-//				clickedLetter->IsHighlighted = true;
-//				CheckForWords();
-//			}
-//		}
-//
-//		void CheckForWords() {
-//			for (int i = 0; i < hiddenWords->Length; i++) {
-//				if (IsWordFound(hiddenWords[i]) && foundWords[i] == nullptr) {
-//					MessageBox::Show("Found word: " + hiddenWords[i]);
-//					foundWords[i] = hiddenWords[i];
-//				}
-//			}
-//		}
-//
-//		bool IsWordFound(String^ word) {
-//			String^ highlightedLetters = "";
-//
-//			for (int i = 0; i < 3; i++) {
-//				for (int j = 0; j < 3; j++) {
-//					if (puzzleGrid[i, j]->IsHighlighted) {
-//						highlightedLetters += puzzleGrid[i, j]->Text;
-//					}
-//				}
-//			}
-//
-//			return highlightedLetters->Equals(word);
-//		}
-//
-//#pragma region Windows Form Designer generated code
-//		/// <summary>
-//		/// Required method for Designer support - do not modify
-//		/// the contents of this method with the code editor.
-//		/// </summary>
-//		void InitializeComponent(void)
-//		{
-//			this->SuspendLayout();
-//			// 
-//			// Form1
-//			// 
-//			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-//			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-//			this->ClientSize = System::Drawing::Size(300, 300);
-//			this->Name = L"Form1";
-//			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-//			this->Text = L"Form1";
-//			this->ResumeLayout(false);
-//
-//		}
-//#pragma endregion
-//	};
-//}
-
 #pragma once
 
 #include <string>
@@ -150,11 +17,14 @@ namespace tts {
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
 	public:
-		Form1(void)
+		Form1(String^ difficulty, String^ category)
 		{
+			// Mengambil string yang dipassing dari form sebelumnya
+			this->difficulty = difficulty;
+			this->category = category;
+
 			InitializeComponent();
 			Text = "Word Puzzle Game";
-			//Size = System::Drawing::Size(1000, 1000);
 			InitializeHiddenWords();
 			InitializePuzzleGrid();
 		}
@@ -172,38 +42,177 @@ namespace tts {
 		System::ComponentModel::Container^ components;
 		array<PuzzleLetter^, 2>^ puzzleGrid;
 		array<String^>^ hiddenWords;
+		String^ difficulty;
+		String^ category;
+
 	private: System::Windows::Forms::FontDialog^ fontDialog1;
 		   array<String^>^ foundWords;
 
 		   void InitializeHiddenWords() {
-			   hiddenWords = gcnew array<String^>{"CAT", "DOG", "RAT"};
+			   hiddenWords = gcnew array<String^>{
+				   "CAT",
+					   "DOG",
+					   "RAT",
+					   "DEER",
+					   "MOUSE",
+					   "SNAKE"
+			   };
 			   foundWords = gcnew array<String^>(hiddenWords->Length);
 		   }
 
 		   void InitializePuzzleGrid() {
-			   puzzleGrid = gcnew array<PuzzleLetter^, 2>(3, 3);
+			   // Deklarasi ukuran puzzle sesuai dengan tingkat difficulty
+			   int puzzle_dimension = 6;
+			   if (difficulty == "Easy") {
+				   puzzle_dimension = 6;
+			   }
+			   else if (difficulty == "Medium") {
+				   puzzle_dimension = 10;
+			   }
+			   else if (difficulty == "Hard") {
+				   puzzle_dimension = 15;
+			   }
 
-			   int idx = 0;
-			   for each (String^ word in hiddenWords)
-			   {
-				   for each(char letter in word)
-				   {
-					   puzzleGrid[idx / 3, idx % 3] = gcnew PuzzleLetter();
-					   puzzleGrid[idx / 3, idx % 3]->Text = Char::ToString(letter);
-					   puzzleGrid[idx / 3, idx % 3]->Size = System::Drawing::Size(50, 50);
-					   puzzleGrid[idx / 3, idx % 3]->BackColor = System::Drawing::Color::White;
-					   puzzleGrid[idx / 3, idx % 3]->Location = Point(50 * (idx % 3) + 50, 50 * (idx / 3) + 50);
-					   puzzleGrid[idx / 3, idx % 3]->Click += gcnew EventHandler(this, &Form1::PuzzleLetter_Click);
-					   puzzleGrid[idx / 3, idx % 3]->Visible = true;
-					   Controls->Add(puzzleGrid[idx / 3, idx % 3]);
-					   puzzleGrid[idx / 3, idx % 3]->BringToFront();
+			   // Inisialisasi array 2 dimensi untuk puzzle
+			   puzzleGrid = gcnew array<PuzzleLetter^, 2>(puzzle_dimension, puzzle_dimension);
+			   for each (PuzzleLetter ^ letter in puzzleGrid) {
+				   letter = gcnew PuzzleLetter();
+				   letter->Text = "";
+				   letter->Size = System::Drawing::Size(50, 50);
+				   letter->BackColor = System::Drawing::Color::White;
+				   //letter->Location = Point(50 * j + 50, 50 * i + 50);
+				   letter->Click += gcnew EventHandler(this, &Form1::PuzzleLetter_Click);
+			   }
+
+			   for each (String ^ word in hiddenWords) {
+				   int max_position = puzzle_dimension - word->Length;
+				   int x = 0;
+				   int y = 0;
+				   int direction = 0;
+				   bool isValid;
+				   int tries = 0;
+				   do {
+					   x = rand() % max_position;
+					   y = rand() % max_position;
+					   direction = rand() % 3;
+
+					   // Check if the position is already occupied
+					   isValid = true;
+					   if (x > max_position || y > max_position) {
+						   if (direction == 2) {
+							   isValid = false;
+						   }
+					   }
+					   if (puzzleGrid[x, y] != nullptr) {
+						   if (puzzleGrid[x, y]->Text != word[0].ToString()) {
+							   isValid = false;
+						   }
+					   }
+					   if (direction == 0) {
+						   for (int i = 0; i < word->Length; i++) {
+							   if (puzzleGrid[x + i, y] != nullptr) {
+								   if (puzzleGrid[x + i, y]->Text != word[i].ToString()) {
+									   isValid = false;
+								   }
+							   }
+						   }
+					   }
+					   else if (direction == 1) {
+						   for (int i = 0; i < word->Length; i++) {
+							   if (puzzleGrid[x, y + i] != nullptr) {
+								   if (puzzleGrid[x, y + i]->Text != word[i].ToString()) {
+									   isValid = false;
+								   }
+							   }
+						   }
+					   }
+					   else if (direction == 2) {
+						   for (int i = 0; i < word->Length; i++) {
+							   if (puzzleGrid[x + i, y + i] != nullptr) {
+								   if (puzzleGrid[x + i, y + i]->Text != word[i].ToString()) {
+									   isValid = false;
+								   }
+							   }
+						   }
+					   }
+
+					   tries++;
+					   if (tries == 10) goto nextWord;
+				   } while (!isValid);
+
+				   puzzleGrid[x, y] = gcnew PuzzleLetter();
+				   puzzleGrid[x, y]->Text = Char::ToString(word[0]);
+				   puzzleGrid[x, y]->Size = System::Drawing::Size(50, 50);
+				   puzzleGrid[x, y]->BackColor = System::Drawing::Color::White;
+				   puzzleGrid[x, y]->Location = Point(50 * y + 50, 50 * x + 50);
+				   puzzleGrid[x, y]->Click += gcnew EventHandler(this, &Form1::PuzzleLetter_Click);
+				   puzzleGrid[x, y]->Visible = true;
+				   Controls->Add(puzzleGrid[x, y]);
+				   puzzleGrid[x, y]->BringToFront();
+
+
+
+				   for each (char letter in word) {
+					   puzzleGrid[x, y] = gcnew PuzzleLetter();
+					   puzzleGrid[x, y]->Text = Char::ToString(letter);
+					   puzzleGrid[x, y]->Size = System::Drawing::Size(50, 50);
+					   puzzleGrid[x, y]->BackColor = System::Drawing::Color::White;
+					   puzzleGrid[x, y]->Location = Point(50 * y + 50, 50 * x + 50);
+					   puzzleGrid[x, y]->Click += gcnew EventHandler(this, &Form1::PuzzleLetter_Click);
+					   puzzleGrid[x, y]->Visible = true;
+					   Controls->Add(puzzleGrid[x, y]);
+					   puzzleGrid[x, y]->BringToFront();
 
 					   // Increment the letter for the next iteration
-					   idx++;
+					   if (direction == 0) {
+						   x++;
+					   }
+					   else if (direction == 1) {
+						   y++;
+					   }
+					   else if (direction == 2) {
+						   x++;
+						   y++;
+					   }
+				   }
+
+			   nextWord:;
+			   }
+
+			   for (int i = 0; i < puzzle_dimension; i++) {
+				   for (int j = 0; j < puzzle_dimension; j++) {
+					   if (puzzleGrid[i, j] == nullptr) {
+						   puzzleGrid[i, j] = gcnew PuzzleLetter();
+						   puzzleGrid[i, j]->Size = System::Drawing::Size(50, 50);
+						   puzzleGrid[i, j]->Location = Point(50 * j + 50, 50 * i + 50);
+						   puzzleGrid[i, j]->Visible = true;  // Ensure visibility
+						   Controls->Add(puzzleGrid[i, j]);
+						   puzzleGrid[i, j]->BringToFront();
+					   }
 				   }
 			   }
-			   array<char, 1>^ letters = gcnew array<char, 1>{'D', 'O', 'G', 'C', 'A', 'T', 'R', 'A', 'T'};
-			   char letter = 'A';
+
+			   //int idx = 0;
+			   //for each (String ^ word in hiddenWords)
+			   //{
+				  // for each (char letter in word)
+				  // {
+					 //  puzzleGrid[idx / 3, idx % 3] = gcnew PuzzleLetter();
+					 //  puzzleGrid[idx / 3, idx % 3]->Text = Char::ToString(letter);
+					 //  puzzleGrid[idx / 3, idx % 3]->Size = System::Drawing::Size(50, 50);
+					 //  puzzleGrid[idx / 3, idx % 3]->BackColor = System::Drawing::Color::White;
+					 //  puzzleGrid[idx / 3, idx % 3]->Location = Point(50 * (idx % 3) + 50, 50 * (idx / 3) + 50);
+					 //  puzzleGrid[idx / 3, idx % 3]->Click += gcnew EventHandler(this, &Form1::PuzzleLetter_Click);
+					 //  puzzleGrid[idx / 3, idx % 3]->Visible = true;
+					 //  Controls->Add(puzzleGrid[idx / 3, idx % 3]);
+					 //  puzzleGrid[idx / 3, idx % 3]->BringToFront();
+
+					 //  // Increment the letter for the next iteration
+					 //  idx++;
+				  // }
+			   //}
+			   //array<char, 1>^ letters = gcnew array<char, 1>{'D', 'O', 'G', 'C', 'A', 'T', 'R', 'A', 'T'};
+			   //char letter = 'A';
 
 			   //for (int i = 0; i < 3; i++) {
 				  // for (int j = 0; j < 3; j++) {
@@ -301,8 +310,8 @@ namespace tts {
 		   bool IsWordFound(String^ word) {
 			   String^ highlightedLetters = "";
 
-			   for (int i = 0; i < 3; i++) {
-				   for (int j = 0; j < 3; j++) {
+			   for (int i = 0; i < puzzleGrid->GetLength(0); i++) {
+				   for (int j = 0; j < puzzleGrid->GetLength(1); j++) {
 					   if (puzzleGrid[i, j]->IsHighlighted) {
 						   highlightedLetters += puzzleGrid[i, j]->Text;
 					   }
